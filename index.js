a5995a86275bdf6e1e575e85244fa6f3efe363fb
EdithWake <105552430+EdithWake@users.noreply.github.com>
2022-05-15 20:28:07 +0530

    1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import highlighter from './highlighter.js'
 function high(){
-    highlighter(document.getElementById("head5"), {backgroundColor="red", flashFor=3000})
+    highlighter(document.getElementById("head5"), {backgroundColor:"red", flashFor:3000})
 }
